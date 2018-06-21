a587b86c5eb3d88c665db1c3f80af388633106ab
chandra1960 <38549511+chandra1960@users.noreply.github.com>
2018-06-21 00:56:21 -0500

    Add files via upload

---
+++ SPringMVCFormHibernate/src/com/spring/dao/EmployeeDao.java
@@ -0,0 +1,20 @@
+package com.spring.dao;
+
+import java.util.List;
+
+import com.spring.model.Employee;
+
+/**
+ * @author ShABBIR aLI
+ *
+ */
+public interface EmployeeDao {
+ 
+ public void addEmployee(Employee employee);
+
+ public List<Employee> listEmployeess();
+ 
+ public Employee getEmployee(int empid);
+ 
+ public void deleteEmployee(Employee employee);
+}
